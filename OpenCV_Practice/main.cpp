0c0caea6fe561549b6ebec8fa58eabfa69b026a4
duckll <a347liao@gmail.com>
2017-02-03 11:21:58 +0800

    Initial Commit

---
+++ OpenCV_Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_Practice
+//
+//  Created by 張良瑋 on 2017/2/3.
+//  Copyright © 2017年 張良瑋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
